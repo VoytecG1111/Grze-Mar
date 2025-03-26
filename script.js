3a7e254b3a505f31fde2b7fa6699c719a5d8829d
Wojciech Guja <114917829+VoytecG1111@users.noreply.github.com>
2025-03-26 12:48:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const galleryBtn = document.getElementById("galeriaBtn");
+    const columny1 = document.getElementById("columny");
+    const carouselContainer = document.getElementById("carouselContainer");
+
+    if (galleryBtn && carouselContainer && columny1) {
+        galleryBtn.addEventListener("click", function(event) {
+            event.preventDefault();
+            columny1.style.visibility ="hidden";
+            columny1.style.opacity = "0";
+            columny1.style.display = "none";
+            carouselContainer.style.visibility = "visible";
+            carouselContainer.style.opacity = "1";
+            carouselContainer.style.display = "flex"; // Ensure it follows Bootstrap layout
+        });
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const homeBtn = document.getElementById("homeBtn");
+    const columny1 = document.getElementById("columny");
+    
+
+    if (homeBtn && carouselContainer && columny1) {
+        homeBtn.addEventListener("click", function(event) {
+            carouselContainer.style.opacity = "0"; // Show the carousel
+            columny1.style.visibility ="visible";
+            columny1.style.opacity = "1";
+            columny1.style.display = "block";
+            columny1.style.display = "flex";
+        });
+    }
+});
+
+document.querySelectorAll('.carousel-item-custom').forEach(item => {
+    item.addEventListener('click', function() {
+        if (window.innerWidth <= 768) { // Only for mobile
+            this.classList.toggle('expanded');
+        }
+    });
+});
